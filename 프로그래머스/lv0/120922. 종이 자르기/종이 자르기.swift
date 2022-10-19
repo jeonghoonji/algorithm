8c050263f0c0bc6a8252f77d1650af1536db2929
jeonghoonji <73868968+jeonghoonji@users.noreply.github.com>
2022-10-19 20:34:16 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120922. 종이 자르기/종이 자르기.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func solution(_ M:Int, _ N:Int) -> Int {
+    return M * N - 1
+//     var m : Int = M
+//     var n : Int = N
+    
+//     var count1 : Int = 0
+//     var count2 : Int = 0
+    
+//     if m==1 && n==1{
+//         return 0
+//     }
+
+//     repeat{
+//         m -= 1 
+//         count1 += 1
+//     }while m > 1
+    
+//     repeat{
+//         n -= 1
+//         count2 += 1
+//     }while n > 1
+
+//     return count1 + (count2 * M)
+}
+
