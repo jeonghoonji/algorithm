f5906a272bce3a7b6a780a1d066194d2ab4c9431
jeonghoonji <73868968+jeonghoonji@users.noreply.github.com>
2022-11-09 20:01:29 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ order:Int) -> Int {
+    var count : Int = 0
+    for i in String(order) {
+        if (i == "3") || (i == "6") || (i == "9"){
+            count += 1
+        }
+        
+    }
+    return count
+}
