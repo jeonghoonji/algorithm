dbaa4597f30fa4042ed6826b51460c4cc6227d44
지정훈 <73868968+jeonghoonji@users.noreply.github.com>
2023-06-06 17:57:52 +0900

    [Silver IV] Title: 학생 번호, Time: 32 ms, Memory: 69264 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.swift
@@ -0,0 +1,25 @@
+let input = Int(readLine()!)!
+var studentList : [String] = []
+var result : Int = 0
+
+for _ in 0..<input{
+    let str = String(readLine()!)
+    studentList.append(str)
+}
+
+let length = studentList[0].count
+
+for i in (0..<length).reversed(){
+    var temp = Set<String>()
+    for j in studentList{
+        let startIndex = j.index(j.startIndex, offsetBy: i)
+        let endIndex = j.index(j.startIndex, offsetBy: length)
+        let subString = j[startIndex..<endIndex]
+        temp.insert(String(subString))
+    }
+    if temp.count == studentList.count{
+        result = length - i
+        break
+    }
+}
+print(result)
