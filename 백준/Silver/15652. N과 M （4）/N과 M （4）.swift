234d93c3898fcc492d632229f6ed698268fd5420
지정훈 <73868968+jeonghoonji@users.noreply.github.com>
2023-06-12 20:34:57 +0900

    [Silver III] Title: N과 M (4), Time: 32 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.swift
@@ -0,0 +1,21 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let N = input[0]
+let M = input[1]
+
+var stack : [Int] = []
+
+dfs(number : 1)
+
+func dfs(number : Int){
+    if stack.count == M {
+        print(stack.map({ String($0)}).joined(separator: " "))
+        return
+    }
+    for i in number..<N+1{
+   
+        stack.append(i)
+        dfs(number : i)
+        stack.removeLast()
+    
+    }
+}
