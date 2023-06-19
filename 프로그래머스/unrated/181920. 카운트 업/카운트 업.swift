0f6adda36d39c383e2393c29a21b2b33a89734bb
지정훈 <73868968+jeonghoonji@users.noreply.github.com>
2023-06-19 16:50:58 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ start:Int, _ end:Int) -> [Int] {
+    var result : [Int] = []
+    for i in start...end{
+        result.append(i)
+    }
+    
+    return result
+}
