f4d44fb9da6dfbfdedbfbab64b7738c2664048b0
지정훈 <73868968+jeonghoonji@users.noreply.github.com>
2023-11-11 22:25:25 +0900

    [Silver III] Title: 병든 나이트, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.swift
@@ -0,0 +1,13 @@
+let NM = readLine()!.split(separator: " ").map{ Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+
+if N == 1{
+    print(1)
+}else if N == 2{
+    print(min(4,(M+1)/2))
+}else if M < 7 {
+    print(min(4,M))
+}else{
+    print(M-2)
+}
